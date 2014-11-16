77cb082c6c1de7d9ea96760258cb5aad60b537c4
Sanders Lauture <golf1052@live.com>
2014-11-16 18:23:31 -0500

    Particle test
    Particle test

--- FlixelPush3/Game1.cs
+++ FlixelPush3/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using GLX;
@@ -15,6 +16,8 @@ namespace FlixelPush3
 
         World world;
         GameTimeWrapper mainGameTime;
+
+        Particle[] particles;
 
         public Game1()
             : base()
@@ -47,7 +50,11 @@ namespace FlixelPush3
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            particles = new Particle[500];
+            for (int i = 0; i < 500; i++)
+            {
+                particles[i] = new Particle(graphics);
+            }
         }
 
         /// <summary>
@@ -71,6 +78,33 @@ namespace FlixelPush3
 
         public void MainUpdate(GameTimeWrapper gameTime)
         {
+            foreach (Particle particle in particles)
+            {
+                if (particle.SpawnParticle(new Vector2(graphics.GraphicsDevice.Viewport.Width / 2,
+                    graphics.GraphicsDevice.Viewport.Height / 2),
+                    Color.White,
+                    new Tuple<int, int>(1000, 2000),
+                    new Tuple<int, int>(3, 3),
+                    new Tuple<float, float>(7, 10),
+                    new Tuple<float, float>(0.95f, 0.99f),
+                    new Tuple<float, float>(0.01f, 0.05f),
+                    new Tuple<float, float>(0.01f, 0.01f),
+                    0,
+                    180,
+                    Color.Yellow,
+                    true,
+                    0.8f,
+                    0.5f))
+                {
+                    break;
+                }
+            }
+
+            foreach (Particle particle in particles)
+            {
+                particle.Update(gameTime, graphics);
+            }
+
             base.Update(gameTime);
         }
 
@@ -82,7 +116,12 @@ namespace FlixelPush3
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            foreach (Particle particle in particles)
+            {
+                particle.Draw(spriteBatch);
+            }
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
